de806431344737aaceec51e9dc08e9e10ada3047
mohamedaliarafat <mohamedaliarafat21@gmail.com>
2025-12-01 14:39:21 +0300

    تحديث الملفات الأخيرة

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2455891154"
+    serviceWorkerVersion: "1224900153"
   }
 });
